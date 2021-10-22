a96c40ef6054fa0cb37f55f730fe7a3af23fcff7
Iram K <khan.iram02@gmail.com>
2021-10-22 10:01:19 +0530

    Updated Exo library, Added Test Case for Date

--- app/src/test/java/com/iram/playeventvideos/ExampleUnitTest.kt
+++ app/src/test/java/com/iram/playeventvideos/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.iram.playeventvideos
 
+import com.iram.playeventvideos.utils.DateFormat
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,10 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun dateCheck(){
+        assertEquals("true", DateFormat.getTomorrowsDate(DateFormat.stringToDate("2021-10-23T04:06:25.232Z")))
     }
 }
